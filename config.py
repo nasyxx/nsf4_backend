6f98918e76c24f3754361fdbdc0a64ab60cdbaa6
Nasy <nasyxx+git@gmail.com>
2020-01-14 11:43:49 -0500

    Change config for Water quality.

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 DB_PATH = "db"
 
-INDEX = "nsf4"
+# INDEX = "nsf4"
+INDEX = "wq"
 
-BASE = "../data/docs"
-DOCS = (
-    "Pacific-Northwest-Coast-Landscape-Conservation-Design_June15-1.txt",
-    "CCLC-Targets-KEAs-Spatial-Design-Criteria-8-4-17-1.txt",
-    "WORKING LANDS AND CONSERVATION COMMUNITY MEETING_Chehalis_2Nov2018.txt",
-    "CONSERVATIONANDWORKINGLANDSCAPES_Warrentonv3_26March2019.txt",
-    "PlanningToolsAssessment2019.txt",
-)
+# BASE = "../data/docs"
+BASE = "../data"
+
+# DOCS = (
+#     "Pacific-Northwest-Coast-Landscape-Conservation-Design_June15-1.txt",
+#     "CCLC-Targets-KEAs-Spatial-Design-Criteria-8-4-17-1.txt",
+#     "WORKING LANDS AND CONSERVATION COMMUNITY MEETING_Chehalis_2Nov2018.txt",
+#     "CONSERVATIONANDWORKINGLANDSCAPES_Warrentonv3_26March2019.txt",
+#     "PlanningToolsAssessment2019.txt",
+# )
+DOCS = ("water_quality.csv",)
