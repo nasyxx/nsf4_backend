fbd17d89a18da0ac32921dbf180010b52add7a52
Nasy <nasyxx+git@gmail.com>
2020-03-02 14:31:45 -0500

    New owl finder.

--- owl.py
+++ owl.py
@@ -44,32 +44,45 @@ Which to you shall seem probable, of every
 # Standard Library
 import re
 from functools import lru_cache
-from itertools import groupby
+from itertools import chain
 
 # Others
-from rdflib import Graph, Literal, URIRef
+from rdflib import Graph, URIRef
 
 # Config
 from config import OWLF
 
 # Types
-from typing import Dict, List, NamedTuple, Set, Union
+from typing import Dict, Generator, NamedTuple, Set, Union
 
 CACHE_SIZE = 2 << 16  # noqa: WPS432
 EMPTYS = ""
+PID = "pid"
 G = Graph().parse(OWLF.as_posix(), format="n3")  # noqa: WPS111
 
-PO = NamedTuple("PO", [("name", str), ("url", str)])
+PO = NamedTuple(
+    "PO",
+    [
+        (PID, str),
+        ("label", str),
+        ("url", str),
+        ("may_answer", str),
+        ("creator", str),
+    ],
+)
+EPO = PO(EMPTYS, EMPTYS, EMPTYS, EMPTYS, EMPTYS)
 
 Person = NamedTuple(
     "Person",
     [
+        (PID, str),
         ("firstname", str),
         ("lastname", str),
         ("job_title", str),
         ("homepage", str),
         ("works_at", PO),
         ("works_on", PO),
+        ("may_answer", str),
     ],
 )
 
@@ -78,62 +91,107 @@ def person_to_dict(person: Person) -> Dict[str, Union[str, Dict[str, str]]]:
     """Person to dict."""
     return dict(
         person._asdict(),
-        works_at=person._asdict()["works_at"]._asdict(),
-        works_on=person._asdict()["works_on"]._asdict(),
+        works_at=person.works_at._asdict(),
+        works_on=person.works_on._asdict(),
     )
 
 
 @lru_cache(maxsize=CACHE_SIZE)
-def build_project_organization() -> Dict[URIRef, PO]:
+def build_other() -> Dict[URIRef, PO]:
     """Build project and organization informatino."""
     return dict(
         map(
             lambda ans: (
-                ans.pid,
-                PO(
-                    str(ans.asdict().get("name", EMPTYS)),
-                    str(ans.asdict().get("url", EMPTYS)),
-                ),
-            ),
+                lambda ansd: (
+                    ansd.get(PID, EMPTYS),
+                    PO(
+                        str(ansd.get(PID, EMPTYS)),
+                        str(
+                            ansd.get(
+                                "name",
+                                ansd.get("title", ansd.get("label", "")),
+                            )
+                        ),
+                        str(ansd.get("url", EMPTYS)),
+                        str(ansd.get("qid", EMPTYS)),
+                        str(ansd.get("creator", EMPTYS)),
+                    ),
+                )
+            )(ans.asdict()),
             G.query(
-                "select ?pid ?id ?name ?label ?url "
+                "select distinct ?pid ?name ?title ?label ?url ?qid ?creator "
                 "where {"
-                "{?pid rdf:type foaf:Organization} "
+                "{?pid rdf:type <http://xmlns.com/foaf/0.1/Organization>} "
                 "union {"
                 "?pid rdf:type ?core "
                 "filter "
                 "(regex(str(?core), 'http://vivoweb.org/ontology/core'))"
+                "} union {"
+                "?pid rdf:type ?report "
+                "filter "
+                "(regex(str(?report), 'http://purl.org/ontology/bibo'))"
                 "} . "
-                "?pid terms:identifier ?id . "
-                "?pid foaf:name ?name . "
-                "?pid rdfs:label ?label "
-                "OPTIONAL {?pid vocab:hasURL ?url} "
-                "}"
+                "?pid sdso:mayAnswer ?qid . "
+                "OPTIONAL {?pid <http://xmlns.com/foaf/0.1/name> ?name} "
+                "OPTIONAL {?pid <http://purl.org/dc/terms/title> ?title} "
+                "OPTIONAL {?pid rdfs:label ?label} "
+                "OPTIONAL {"
+                "?pid <http://dev.poderopedia.com/vocab/hasURL> ?url"
+                "} "
+                "OPTIONAL {"
+                "?pid <http://purl.org/dc/terms/creator> ?creator"
+                "} "
+                "}",
             ),
         )
     )
 
 
 @lru_cache(maxsize=CACHE_SIZE)
-def build_person() -> List[Dict[str, Union[URIRef, Literal]]]:
+def build_person() -> Set[Person]:
     """Build person infomation."""
-    return list(
+    return set(
         map(
-            lambda ans: ans.asdict(),
+            lambda ans: (
+                lambda person: Person(
+                    str(person.get(PID, EMPTYS)),
+                    str(person.get("firstname", EMPTYS)),
+                    str(person.get("lastname", EMPTYS)),
+                    " ".join(
+                        re.findall(  # handle
+                            # Scientist-Ecosystem or
+                            # ScientistEcosystem to
+                            r"([A-Z][a-z,]*)",
+                            #  Scientist Ecosystem
+                            str(person.get("job_title", EMPTYS)),
+                        )
+                    ),
+                    str(person.get("homepage", EMPTYS)),
+                    build_other().get(person.get("works_at"), EPO),
+                    build_other().get(person.get("works_on"), EPO),
+                    str(person.get("qid", EMPTYS)),
+                )
+            )(ans.asdict()),
             G.query(
-                "select distinct ?pid ?id "
+                "select ?pid "
                 "?firstname ?lastname ?job_title "
-                "?homepage ?label ?works_on ?works_at "
+                "?homepage ?label ?works_on ?works_at ?qid "
                 "where {"
-                "?pid rdf:type foaf:Person . "
-                "?pid terms:identifier ?id "
+                "?pid rdf:type <http://xmlns.com/foaf/0.1/Person> . "
                 "OPTIONAL {?pid sdso:holdsJobTitle ?job_title } "
                 "OPTIONAL {?pid sdso:worksAt ?works_at } "
                 "OPTIONAL {?pid sdso:worksOn ?works_on } "
-                "OPTIONAL {?pid foaf:firstName ?firstname } "
-                "OPTIONAL {?pid foaf:homepage ?homepage } "
-                "OPTIONAL {?pid foaf:lastName ?lastname } "
-                "OPTIONAL {?pid rdfs:label ?label } "
+                "OPTIONAL {"
+                "?pid <http://xmlns.com/foaf/0.1/firstName> ?firstname "
+                "} "
+                "OPTIONAL {"
+                "?pid <http://xmlns.com/foaf/0.1/homepage> ?homepage "
+                "} "
+                "OPTIONAL {"
+                "?pid <http://xmlns.com/foaf/0.1/lastName> ?lastname "
+                "} "
+                "OPTIONAL {?pid rdfs:label ?label} "
+                "OPTIONAL {?pid sdso:mayAnswer ?qid}"
                 "} "
                 "order by ?pid"
             ),
@@ -141,45 +199,26 @@ def build_person() -> List[Dict[str, Union[URIRef, Literal]]]:
     )
 
 
-@lru_cache(maxsize=CACHE_SIZE)
-def person_cache() -> Dict[str, Set[Person]]:
-    """Generate person caches."""
-    return dict(
-        map(
-            lambda keygroup: (
-                keygroup[0],
-                set(
-                    map(
-                        lambda person: Person(
-                            str(person.get("firstname", EMPTYS)),
-                            str(person.get("lastname", EMPTYS)),
-                            re.sub(  # handle
-                                # Scientist-Ecosystem or
-                                # ScientistEcosystem to
-                                r".*#([^-]*).*?([A-Z])",
-                                #  Scientist Ecosystem
-                                r"\1 \2",
-                                str(person.get("job_title", EMPTYS)),
-                            ).strip(),
-                            str(person.get("homepage", EMPTYS)),
-                            build_project_organization().get(
-                                person.get("works_at"), PO(EMPTYS, EMPTYS),
-                            ),
-                            build_project_organization().get(
-                                person.get("works_on"), PO(EMPTYS, EMPTYS)
-                            ),
-                        ),
-                        keygroup[1],
-                    )
-                ),
+def filter_other(other: PO) -> Generator[Person, None, None]:
+    """Filter the person from others."""
+    yield from filter(
+        lambda person: person.works_at == other.pid
+        or person.works_on == other.pid
+        or person.pid == other.creator,
+        build_person(),
+    )
+
+
+def filtered(qid: str) -> Generator[Person, None, None]:
+    """Filtered persons."""
+    yield from chain(
+        filter(lambda person: person.may_answer == qid, build_person()),
+        *map(
+            filter_other,
+            filter(
+                lambda other: other.may_answer == qid, build_other().values()
             ),
-            groupby(
-                build_person(),
-                lambda person: re.sub(
-                    r"(.+?)(0{2}[0-9]{2})?$", r"\1", person.get("pid", EMPTYS)
-                ),
-            ),
-        )
+        ),
     )
 
 
@@ -188,13 +227,11 @@ def query(query_str: str) -> Set[Person]:
     """Query owl graph."""
     return set(
         *map(
-            lambda ans: person_cache().get(str(ans[0])),
+            lambda ans: filtered(str(ans.asdict().get("qid", ""))),
             G.query(
-                "select ?collection where { "
+                "select ?qid where { "
                 # Find the query text id as `qid`.
                 f'?qid sdso:ntkText "{query_str}"^^xsd:string . '
-                # Find the base `collection` of the `qid`
-                "?qid sdso:collectionInstrument ?collection "
                 "}"
             ),
         ),
