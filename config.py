3fba5eba8c2fabaa9a0317c6600623fd2c5f62a4
Nasy <nasyxx+git@gmail.com>
2020-03-02 17:13:48 -0500

    Update owl file path

--- config.py
+++ config.py
@@ -65,7 +65,7 @@ INDEX = "all"
 
 BASE = Path("../data")
 
-OWLF = BASE / "SDS-OKN_20200301_CleanOntology_InstanceTurtle.owl"
+OWLF = BASE / "SDS-OKN_20200301_CleanOntology_InstanceTurtle_PJM_3PM.owl"
 
 DOCS = Docs(
     normal=FD(
